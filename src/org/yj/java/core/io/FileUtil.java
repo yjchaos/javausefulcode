405bfb193ae7dbf39ed4697d3c20d0bd1d2470aa
yaojun <522899547@qq.com>
2019-01-26 15:24:28 +0800

    update

---
+++ src/org/yj/java/core/io/FileUtil.java
@@ -0,0 +1,152 @@
+package org.yj.java.core.io;
+
+import java.io.*;
+
+/**
+ * @author yaojun
+ * @version 1.0
+ * @date 2019/1/26 11:27
+ */
+public class FileUtil {
+
+    public static void main(String[] args) {
+        FileUtil fileUtil = new FileUtil();
+        long beforTime = System.currentTimeMillis();
+        // fileUtil.copyWithStream("src/org/yj/java/core/io/directory4Test/video.mp4", "src/org/yj/java/core/io/directory4Test/video_copy.mp4");
+        // fileUtil.copyWithBufferdStream("src/org/yj/java/core/io/directory4Test/video.mp4",
+        // "src/org/yj/java/core/io/directory4Test/video_copy.mp4");
+        fileUtil.writeTest("src/org/yj/java/core/io/directory4Test/writeTest.txt");
+        long afterTime = System.currentTimeMillis();
+        System.out.println(afterTime - beforTime);
+    }
+
+    public void writeTest(String dest) {
+        File destFile = new File(dest);
+        if (!destFile.exists()) {
+            try {
+                boolean flag = destFile.createNewFile();
+                if (flag) {
+                    System.out.println("dest file create success");
+                } else {
+                    System.out.println("dest file create fail");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        OutputStream os = null;
+        try {
+            os = new BufferedOutputStream(new FileOutputStream(destFile));
+            for (int i = 0; i < 10000; i++) {
+                os.write(("this is line:" + i + "\r\n").getBytes());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    public void copyWithStream(String src, String dest) {
+        File srcFile = new File(src);
+        File destFile = new File(dest);
+        if (!srcFile.exists()) {
+            return;
+        }
+        if (!destFile.exists()) {
+            try {
+                boolean flag = destFile.createNewFile();
+                if (flag) {
+                    System.out.println("dest file create success");
+                } else {
+                    System.out.println("dest file create fail");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(srcFile);
+            os = new FileOutputStream(destFile);
+            byte[] bytes = new byte[1024];
+            int len;
+            while ((len = is.read(bytes)) != -1) {
+                os.write(bytes, 0, len);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != os) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void copyWithBufferdStream(String src, String dest) {
+        File srcFile = new File(src);
+        File destFile = new File(dest);
+        if (!srcFile.exists()) {
+            return;
+        }
+        if (!destFile.exists()) {
+            try {
+                boolean flag = destFile.createNewFile();
+                if (flag) {
+                    System.out.println("dest file create success");
+                } else {
+                    System.out.println("dest file create fail");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new BufferedInputStream(new FileInputStream(srcFile));
+            os = new BufferedOutputStream(new FileOutputStream(destFile));
+            byte[] bytes = new byte[1024];
+            int len;
+            while ((len = is.read(bytes)) != -1) {
+                os.write(bytes, 0, len);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != os) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
